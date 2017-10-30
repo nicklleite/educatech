ca7023ad6840a9f208c762168ff4b366d581aaed
Nicholas Leite <nicklleite@gmail.com>
2017-10-30 07:31:45 -0200

    Alterações nas rotas, adicionando os métodos HTTP

--- application/controllers/DominioApiController.php
+++ application/controllers/DominioApiController.php
@@ -1,34 +1,29 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-require_once("ApiController.php");
-
-class DominioApiController extends ApiController {
+class DominioApiController extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
-
-        ApiController::init(FALSE);
         $this->load->model('DominioModel', 'dominio', TRUE);
     }
 
-    public function index() {
-        redirect(base_url() . 'api/dominio/todos');
-    }
+    public function getAll() {
+        header("Content-Type: application/json");
 
-    public function buscarTodos() {
         $q = $this->dominio->buscar();
         $data = array();
         
         if ($q->num_rows() > 0) {
-            foreach ($q->result() as $row) {
-                $q2 = $this->dominio->buscarPorDescr($row->dominio);
-                $data[$row->dominio] = array();
+            // Exibe os dados agrupados por domínio.
+            // foreach ($q->result() as $row) {
+            //     $q2 = $this->dominio->buscarPorDescr($row->dominio);
+            //     $data[$row->dominio] = array();
 
-                foreach ($q2->result() as $row2) {
-                    array_push($data[$row->dominio], array($row2->vl => $row2->descr));
-                }
-            }
-            // $data = $q->result_array();
+            //     foreach ($q2->result() as $row2) {
+            //         array_push($data[$row->dominio], array($row2->vl => $row2->descr));
+            //     }
+            // }
+            $data = $q->result_array();
         } else {
             $data["resposta"] = 404;
             $data["mensagem"] = "Nenhum registro encontrado!";
@@ -37,7 +32,7 @@ class DominioApiController extends ApiController {
         echo json_encode($data);
     }
 
-    public function buscarPorDescr($dominio) {
+    public function getByDominio($dominio) {
         header("Content-Type: application/json");
 
         $data = array();
