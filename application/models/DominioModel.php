891c0bda0d12431440e054e25d5fd42828319438
Nicholas Leite <nicklleite@gmail.com>
2017-10-30 16:49:55 -0200

    Documentação em comentários

--- application/models/DominioModel.php
+++ application/models/DominioModel.php
@@ -1,11 +1,36 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 class DominioModel extends CI_Model {
+
+    /**
+     * Método para consulta e retorno dos registros da tabela DOMINIO
+     * 
+     * @return CI_DB_result
+     * @see https://www.codeigniter.com/userguide3/database/query_builder.html
+     *
+     * @author Nicholas Leite <nicklleite@gmail.com>
+     * @package application\models
+     * @since v0.0.0
+     * @version v0.0.3
+     */
     public function buscar() {
         $query = $this->db->get("dominio");
         return $query;
     }
 
+    /**
+     * Função que retorna todos os registros de domínios, com base em um
+     * parâmetro.
+     * 
+     * @param string $dominio
+     * @return CI_DB_result
+     * @see https://www.codeigniter.com/userguide3/database/query_builder.html
+     *
+     * @author Nicholas Leite <nicklleite@gmail.com>
+     * @package application\models
+     * @since v0.0.0
+     * @version v0.0.3
+     */
     public function buscarPorDescr($dominio) {
         $this->db->where("dominio", $dominio);
         $this->db->order_by('vl', 'ASC');
