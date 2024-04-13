4683f8e56dd4488906e7e547430578a84e1cc357
Nicholas Leite <nicholas.lopes@plugg.to>
2024-04-13 04:17:36 -0300

    Refactoring the login feature test; still need some improvement...

--- app/Http/Controllers/Api/v1/LoginController.php
+++ app/Http/Controllers/Api/v1/LoginController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
@@ -13,16 +13,13 @@ class LoginController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(LoginRequest $request)
     {
         $user = User::where('email', $request->email)->first();
 
         if (!$user || !Hash::check($request->password, $user->password))
         {
-
-            throw ValidationException::withMessages([
-                'email' => ['The provided credentials are incorrect.'],
-            ]);
+            throw ValidationException::withMessages(['The provided credentials are incorrect.']);
         }
 
         return response()->json([
--- tests/Feature/LoginModuleTest.php
+++ tests/Feature/LoginModuleTest.php
@@ -1,40 +1,88 @@
 <?php
 use App\Models\User;
 
-use function Pest\Laravel\getJson;
-use function Pest\Laravel\json;
-use function Pest\Laravel\post;
 use function Pest\Laravel\postJson;
 
-beforeEach(function ()
+it("should get a 200 HTTP status on login with valid credentials and the user data should be returned with the auth token", function ()
 {
-    $this->user = User::factory()->create();
-    $this->token = $this->user->createToken("auth_token")->plainTextToken;
-});
+    $user = User::factory()->create();
 
-test("if the login will be successfull, returning the expected data structure", function ()
-{
-    post(route('api.login'), [
-        'email' => $this->user->email,
+    $response = postJson(route("api.login"), [
+        'email' => $user->email,
         'password' => "password",
-    ])->assertJsonStructure([
-                "user" => [
-                    "code",
-                    "name",
-                    "email",
-                    "email_verified_at",
-                    "created_at",
-                    "updated_at",
-                    "deleted_at",
-                ],
-                "token",
-            ])->assertOk();
+    ]);
+
+    expect($response->getStatusCode())
+        ->toBe(200)
+        ->and($response->getContent())
+        ->toBeJson()
+        ->and($response->assertJsonStructure([
+            "user" => [
+                "code",
+                "name",
+                "email",
+                "email_verified_at",
+                "created_at",
+                "updated_at",
+                "deleted_at",
+            ],
+            "token",
+        ]));
+    // TODO arrumar um jeito de criar um usuário temporário, que não atrapalhe os índices da tabela
+    $user->forceDelete();
 });
 
-test("if login fails with incorrect credentials", function ()
-{
-    json("POST", route('api.login'), [
-        'email' => $this->user->email,
-        'password' => "password1",
-    ])->assertUnprocessable();
-});
+// it("should get a 422 HTTP status on login with an empty email and validation error messages should be returned rather than the user data", function ()
+// {
+//     $response = postJson(route("api.login"), [
+//         'email' => "",
+//         'password' => "password",
+//     ]);
+
+//     expect($response->getStatusCode())
+//         ->toBe(422)
+//         ->and($response->getContent())
+//         ->toBeJson()
+//         ->and($response->assertJsonStructure([
+//             "message",
+//             "errors" => [
+//                 "email" => [],
+//             ],
+//         ]));
+// });
+
+// it("should get a 422 HTTP status on login with an empty password and validation error messages should be returned rather than the user data", function ()
+// {
+//     $response = postJson(route("api.login"), [
+//         'email' => $this->user->email,
+//         'password' => "",
+//     ]);
+
+//     expect($response->getStatusCode())
+//         ->toBe(422)
+//         ->and($response->getContent())
+//         ->toBeJson()
+//         ->and($response->assertJsonStructure([
+//             "message",
+//             "errors" => [
+//                 "password" => [],
+//             ],
+//         ]));
+// });
+
+// it("should get a 422 HTTP status on login with invalid credentials and validation error messages should be returned rather than the user data", function ()
+// {
+//     $response = postJson(route("api.login"), [
+//         'email' => $this->user->email,
+//         'password' => "password1",
+//     ]);
+
+//     expect($response->getStatusCode())
+//         ->toBe(422)
+//         ->and($response->getContent())
+//         ->toBeJson()
+//         ->and($response->assertJsonStructure([
+//             "message",
+//             "errors" => [],
+//         ]));
+// });
